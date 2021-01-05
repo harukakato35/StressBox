a2e4260bf77d74e82445b7306b1ba51053746d39
Haruka Kato <samuraiharuka1@gmail.com>
2021-01-05 19:46:01 +0900

    Adding top page

--- src/components/Top.js
+++ src/components/Top.js
@@ -4,11 +4,28 @@ import Header from './Header';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import candles from './Picture/candles.jpg';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
+    box:{
+        position: 'relative',
+    },
+    button:{
+        position: 'absolute',
+        top: '50%',
+        left: '65%',
+        height: 60,
+    },
+    p:{
+        position: 'absolute',
+        top: '10%',
+        left: '65%',
+        height: 60,
+        fontSize: 80,
+    }
 }));
 
 export default function Top() {
@@ -18,13 +35,18 @@ export default function Top() {
         <React.Fragment>
             <Header/>
                 <Box
-                    class="candles"
+                    className={classes.box}
                     style={{
                         backgroundImage: `url(${candles})`,
                         backgroundSize: "cover",
                         height: "100vh",
                         color: "#f5f5f5"
                 }}>
+
+                    <Button variant="contained" color="primary" disableElevation className={classes.button}>
+                        Disable elevation
+                    </Button>
+                    <p className={classes.p}>Picked Just <br></br>For You</p>
                 </Box>
         </React.Fragment>
     );
