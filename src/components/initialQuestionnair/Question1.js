c5fa674de200cea1a380ad50c6fe3e72fbc6abd6
Haruka Kato <samuraiharuka1@gmail.com>
2021-02-12 10:29:31 +0900

    adding questionnair

--- src/components/initialQuestionnair/Question1.js
+++ src/components/initialQuestionnair/Question1.js
@@ -4,11 +4,10 @@ import { Link } from 'react-router-dom'
 import Header from '../BasicComponents/Header';
 import Footer from '../BasicComponents/Footer';
 import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import candles1 from '../Picture/candles1.jpg';
 import Button from '@material-ui/core/Button';
-import { useFirebaseApp } from 'reactfire';
-import candles from "../Picture/candles.jpg";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -18,20 +17,32 @@ const useStyles = makeStyles((theme) => ({
         position: 'relative',
     },
     button:{
-        position: 'absolute',
-        top: '80%',
-        left: '10%',
-        opacity: 0.5,
+        display: 'block',
+        margin: '0 auto',
+        marginBottom: 50,
+        opacity: 0.7,
         height: 60,
         borderRadius: 50,
     },
-    p:{
-        position: 'absolute',
-        top: '1%',
-        left: '5%',
-        height: 60,
-        fontSize: 80,
-    }
+    font:{
+        textAlign: 'center',
+    },
+    paper: {
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        height: 100,
+        margin:5,
+    },
+    grid:{
+        marginTop: 50,
+        justifyContent: 'center',
+        alignContent:  'center',
+    },
+    grid1:{
+        justifyContent: 'center',
+        alignContent:  'center',
+        marginBottom: 50,
+    },
 }));
 
 export default function Question1() {
@@ -45,8 +56,34 @@ export default function Question1() {
                 style={{
                     color: "#white"
                 }}>
+                <h2 className={classes.font}>Your customized stress release plan.</h2>
+                <p className={classes.font}>50% complete</p>
+                <h1 className={classes.font} >How did your stress change over the month?</h1>
+                <h3 className={classes.font}>Select all that apply(required)</h3>
+                    <Grid container className={classes.grid} >
+                        <Grid item xs={6} sm={2} >
+                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                        </Grid>
+                        <Grid item xs={6} sm={2}>
+                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                        </Grid>
+                        <Grid item xs={6} sm={2}>
+                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                        </Grid>
+                    </Grid>
+                    <Grid container className={classes.grid1}>
+                        <Grid item xs={6} sm={2}>
+                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                        </Grid>
+                        <Grid item xs={6} sm={2}>
+                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                        </Grid>
+                        <Grid item xs={6} sm={2}>
+                            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                        </Grid>
+                    </Grid>
                 <Button variant="contained" color="primary" disableElevation className={classes.button}>
-                    <Link to="/question2">⇨　Go to questions</Link>
+                    <Link to="/question2">⇨　Next question</Link>
                 </Button>
             </Box>
             <Footer/>
--- src/components/initialQuestionnair/Question2.js
+++ src/components/initialQuestionnair/Question2.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom'
 import Header from '../BasicComponents/Header';
 import Footer from '../BasicComponents/Footer';
 import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import candles1 from '../Picture/candles1.jpg';
 import Button from '@material-ui/core/Button';
-import { useFirebaseApp } from 'reactfire';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,32 +17,95 @@ const useStyles = makeStyles((theme) => ({
         position: 'relative',
     },
     button:{
-        position: 'absolute',
-        top: '80%',
-        left: '10%',
-        opacity: 0.5,
+        display: 'block',
+        margin: '0 auto',
+        marginBottom: 50,
+        opacity: 0.7,
         height: 60,
         borderRadius: 50,
     },
-    p:{
-        position: 'absolute',
-        top: '1%',
-        left: '5%',
-        height: 60,
-        fontSize: 80,
-    }
+    font:{
+        textAlign: 'center',
+    },
+    paper: {
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        height: 100,
+        margin:5,
+        width: '100%',
+    },
+    grid:{
+        marginTop: 50,
+        justifyContent: 'center',
+        alignContent:  'center',
+    },
+    grid1:{
+        justifyContent: 'center',
+        alignContent:  'center',
+        marginBottom: 50,
+    },
+    blackButton:{
+    backgroundColor: 'black',
+    color: 'white',
+},
+
+whiteButton:{
+    backgroundColor: 'white',
+    color: 'black',
+}
 }));
 
 export default function Question2() {
     const classes = useStyles();
+    const [color,setColor]=useState('#F5F5F5');
+    const [textColor,setTextColor]=useState('white');
+    const button = (
+        <Button style={{background:color,color:textColor}}
+                className={classes.paper}
+                onClick={()=>{setColor("purple");setTextColor('white')
+                }}>
+            asda
+        </Button>
+        )
 
     return (
         <React.Fragment>
             <Header/>
-
-            <Button variant="contained" color="primary" disableElevation className={classes.button}>
-                <Link to="/result">⇨　Go to questions</Link>
-            </Button>
+            <Box
+                className={classes.box}
+                style={{
+                    color: "#white"
+                }}>
+                <h2 className={classes.font}>Your customized stress release plan.</h2>
+                <p className={classes.font}>100% complete</p>
+                <h1 className={classes.font} >How did your stress change over the month?</h1>
+                <h3 className={classes.font}>Select all that apply(required)</h3>
+                <Grid container className={classes.grid} >
+                    <Grid item xs={6} sm={2} >
+                        {button}
+                    </Grid>
+                    <Grid item xs={6} sm={2}>
+                        <Button className={classes.paper}>asda</Button>
+                    </Grid>
+                    <Grid item xs={6} sm={2}>
+                        <Button className={classes.paper}>asda</Button>
+                    </Grid>
+                </Grid>
+                <Grid container className={classes.grid1}>
+                    <Grid item xs={6} sm={2}>
+                        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                    </Grid>
+                    <Grid item xs={6} sm={2}>
+                        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                    </Grid>
+                    <Grid item xs={6} sm={2}>
+                        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                    </Grid>
+                </Grid>
+                <Button variant="contained" color="primary" disableElevation className={classes.button}>
+                    <Link to="/result">⇨　Show the result for me</Link>
+                </Button>
+            </Box>
             <Footer/>
         </React.Fragment>
     );
